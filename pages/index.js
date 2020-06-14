73caeee10f9763bd3247b36c5557e3f0a6fd71e5
José Américo Lacerda Félix <joseamericolf@gmail.com>
2020-06-14 00:49:00 -0300

    teste

--- pages/index.js
+++ pages/index.js
@@ -22,12 +22,8 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>Olá, sou o Zé. Sou bolsista no{' '} 
           <a href="http://nuted.ufrgs.br">Nuted</a>{' '}
-          e participo do desenvolvimento do aplicativo do{' '}
+          participando do desenvolvimento do aplicativo do{' '}
           <a href="https://recoacomp.ufrgs.br">Recoacomp</a>.
-        </p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
