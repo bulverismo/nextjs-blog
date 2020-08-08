bca454cf447f5e0d618096f552a54eedd25b1d3a
José Américo Lacerda Félix <joseamericolf@gmail.com>
2020-08-08 03:43:14 -0300

    mod pagina inicial

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Olá, somos uma empresa nova!</p>
+        <p>Sorria!</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
